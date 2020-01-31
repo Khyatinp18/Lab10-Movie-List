98242cb87ad3819d6f52e39cbdcb1c652e3cca48
khyatinp18 <khyatinp18@gmail.com>
2020-01-30 23:18:27 -0500

    Lab10 Movie List

---
+++ Lab 10 Movie List/Movie.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10_Movie_List
+{
+    class Movie
+    {//variable instance
+        private string title;
+        private string category;
+
+     //Properties
+        public string Title
+        {
+            get {return title;}
+            set {title = value;}
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        //Constructor
+        public Movie()
+        {
+
+        }
+        public Movie (string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+
+        //Method to get movie category
+        public static List<string> GetListOfCategories(List<Movie> categoryList)
+        {
+            List<string> CategoryTypes = new List<string>();
+            foreach (Movie category in categoryList)
+            {
+                if (!CategoryTypes.Contains(category.Category))
+                {
+                    CategoryTypes.Add(category.Category);
+                }
+            }
+            return CategoryTypes;
+        }
+
+
+
+        //Method to get movie category
+        public static void PrintMovieCategory(List<Movie> categoryList)
+        {
+            byte index = 1;
+            List<string> CategoryTypes = GetListOfCategories(categoryList);
+            foreach (string type in CategoryTypes)
+            {
+                Console.WriteLine($"{index}. {type}");
+                index++;
+            }
+            Console.WriteLine();
+        }
+
+
+        //Getting input from user
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+
+
+        ////Method to display movie category
+        public static void PrintMovieByCategory(List<Movie> categoryList, string categorySelected)
+        {
+            List<string> tempList = new List<string>();
+            foreach (Movie movie in categoryList)
+            {
+                if (categorySelected == movie.Category)
+                {
+                    if (movie.Category == "Comedy")
+                    {
+                        tempList.Add(movie.Title);
+                        
+                    }
+                    else if (movie.Category == "Animated")
+                    {
+                        tempList.Add(movie.Title);
+                       
+                    }
+                    else if (movie.Category == "Action")
+                    {
+                        tempList.Add(movie.Title);
+                        
+                    }
+
+                }
+            }
+
+            //print aplphabetically sorted list
+            tempList.Sort();
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                Console.WriteLine(tempList[i]);
+            }
+        }
+
+
+        //Method to ask user if they want to continue
+        public static bool KeepGoing(string message, string option1, string option2)
+        {
+            string keepGoing = GetUserInput(message).ToLower();
+            if (keepGoing == option1)
+            {
+                return false;
+            }
+            else if (keepGoing == option2)
+            {
+                return true;
+            }
+            else
+            {
+                return KeepGoing("Not valid! " + message, option1, option2);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Lab 10 Movie List/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_10_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> MovieList = new List<Movie>
+            {
+                new Movie("Airplane", "Comedy"),
+                new Movie("Home Alone", "Comedy"),
+                new Movie("Mrs. Doubtfire", "Comedy"),
+                new Movie("Frozen", "Animated"),
+                new Movie("Coco", "Animated"),
+                new Movie("Aladdin", "Animated"),
+                new Movie("Terminator", "Action"),
+                new Movie("Rambo", "Action"),
+                new Movie("Black Panther", "Action"),
+                new Movie("The Bourne Ultimatum", "Action"),
+            };
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.ResetColor();
+
+
+            //Program will continue asking user if they enter invalid input and also asks them if they want to know about another movie category 
+            bool wantToContinue = true;
+            bool validEntry = true;
+            int index = 0;
+            while (wantToContinue)
+            {
+                validEntry = true;
+
+                //Display movie category list
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("There are three movie categories in this list:");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Movie.PrintMovieCategory(MovieList);
+                Console.ResetColor();
+
+
+                while (validEntry)
+                {
+                    //Asking user to select category by entering number
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    string userChoice = Movie.GetUserInput("What category are you interested in?\n" +
+                        "Please select the category by entering a number (1-3):");
+                   
+                     index = int.Parse(userChoice);
+                    Console.ResetColor();
+
+                    //validating the user input
+                    if (index < 1 || index > 3)
+                    {
+                        Console.WriteLine("Invalid Entry, please try again.");
+                        validEntry = true;
+                    }
+                    else
+                    {
+                        validEntry = false;
+                    }
+                }
+
+                //Showing the result of movie names based on catagory selsected by user
+                List<string> CategoryTypes = Movie.GetListOfCategories(MovieList);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"You have selected category {CategoryTypes[index - 1]}, Here is the list:");
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Movie.PrintMovieByCategory(MovieList, CategoryTypes[index - 1]);
+                Console.ResetColor();
+
+                
+                //asking user if they want to continue
+                wantToContinue = Movie.KeepGoing($"Would you like to know about another movie category (y/n)?", "n", "y");
+            }
+
+            Console.WriteLine("Thank you for your time, Goodbye!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
